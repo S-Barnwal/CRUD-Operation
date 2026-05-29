4bb92d0bd42b930e029d8e3731d6365162c30f7f
Sneha Barnwal <your-email@gmail.com>
2026-05-29 09:49:20 +0530

    mongodb live url connect

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const port = 3000
 const connectDB = async () => {
     try {
 
-        await mongoose.connect('mongodb://localhost:27017/blogify')
+        await mongoose.connect('mongodb+srv://snehabarnwal984_db_user:rrmNQXf3lleFvWzS@blogifycluster.9k1bl9h.mongodb.net//blogify')
 
         console.log("MongoDB connected. 🥳")
 
